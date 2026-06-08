2f7bd82d5d50257c4452b0cb113cda2a05024667
Tannou-dev <aboutannou0@gmail.com>
2026-06-08 05:42:48 +0000

    fix: use flutter_secure_storage for master password instead of SharedPreferences

--- app_build/macos/Flutter/GeneratedPluginRegistrant.swift
+++ app_build/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,11 +5,13 @@
 import FlutterMacOS
 import Foundation
 
+import flutter_secure_storage_macos
 import local_auth_darwin
 import share_plus
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   LocalAuthPlugin.register(with: registry.registrar(forPlugin: "LocalAuthPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
